0cef34579b2100bb6ba1a0d340ea4839003cc551
genc-v <vllahiu.genc@gmail.com>
2025-12-12 10:10:34 +0100

    delete not to copy

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+
+-- In your keymaps.lua or inside lazy-config opts
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Half-page down centered" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Half-page up centered" })
+
+-- Never yank on delete
+vim.keymap.set({ "n", "v" }, "d", '"_d')
+vim.keymap.set({ "n", "v" }, "D", '"_D')
